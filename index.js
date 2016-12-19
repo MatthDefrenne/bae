9d83692519b6665298fa84f50bd7e7f7f7e26335
Matthieu Defrenne <matthieu.defrenne@gmail.com>
2016-12-19 23:49:09 +0100

    sqdfsqdf

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
+var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-var path = require('path');
-var compression = require('compression');
-var helmet = require('helmet');
 
-var port = process.env.PORT || 8080;
-process.env.PWD = process.cwd();
-console.log(process.env.PWD );
-
-app.use(express.static(path.join(__dirname, 'public'), {maxAge: 86400000}));
-
-app.get('/', function (req, res) {
-    const index = path.join(process.env.PWD, 'public', 'index.html');
-    res.sendFile(index);
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/public"));
+app.listen(process.env.PORT || 5000);
