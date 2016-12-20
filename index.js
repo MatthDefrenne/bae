515d26c9a49728ce8bf12a67da8882789e066ec9
Matthieu Defrenne <matthieu.defrenne@gmail.com>
2016-12-20 01:37:14 +0100

    push

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log(process.env.PWD );
 app.use(express.static("./public"));
 
 app.get('*', function (req, res) {
-    var file = "./public/index.html";
+    var file = "./index.html";
     res.sendFile(path.resolve(file));
 });
 
