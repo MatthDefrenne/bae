1683108a920d92d4cd5e642a56c4d1437361646a
Matthieu Defrenne <matthieu.defrenne@gmail.com>
2016-12-20 02:48:53 +0100

    file

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ var compression = require('compression');
 var helmet = require('helmet');
 
 var port = process.env.PORT || 8080;
-process.env.PWD = process.cwd();
-console.log(process.env.PWD );
 
-app.use(express.static(process.env.PWD + '/client'));
+
+app.use(helmet());
+
+app.use(express.static("client"));
 
 app.get('*', function (req, res) {
-    var file = process.env.PWD + "/client/index.html";
+    var file = __dirname + "/clieent/index.html";
     res.sendFile(path.resolve(file));
 });
+
 
 
 app.listen(port, function() {
