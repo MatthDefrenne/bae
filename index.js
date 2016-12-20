cb7c58cf61a482575416e3de3d4fdcdbd0ca6413
Matthieu Defrenne <matthieu.defrenne@gmail.com>
2016-12-20 02:20:34 +0100

    ded

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ var port = process.env.PORT || 8080;
 process.env.PWD = process.cwd();
 console.log(process.env.PWD );
 
-app.use(express.static(path.join(process.cwd(), '../../speaky-client/'), {maxAge: 86400000}));
+app.use(express.static("/public"));
 
 app.get('*', function (req, res) {
-    var file = path.join('./drinky', '/public/', 'index.html');
-    return res.sendfile(path.resolve(file));
+    var file = __dirname + "/public/index.html";
+    res.sendFile(path.resolve(file));
 });
 
 
-app.listen(port, '137.74.164.253', function() {
+app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
