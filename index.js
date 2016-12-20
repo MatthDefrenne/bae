239d85dd1c50921ca302fade2c9fed595b77f329
Matthieu Defrenne <matthieu.defrenne@gmail.com>
2016-12-20 02:10:53 +0100

    qsqsd

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log(process.env.PWD );
 app.use(express.static(path.join(process.cwd(), '../../public'), {maxAge: 86400000}));
 
 app.get('*', function (req, res) {
-    var file = path.join(process.cwd(), '/drinky/public/index.html');
+    var file = path.join(process.cwd(), '../public/index.html');
     return res.sendFile(path.resolve(file));
 });
 
