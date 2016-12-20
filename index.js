a9f2789879319acc93ffab7b11a7bfac10c020b7
Matthieu Defrenne <matthieu.defrenne@gmail.com>
2016-12-20 02:09:11 +0100

    qsd

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var port = process.env.PORT || 8080;
 process.env.PWD = process.cwd();
 console.log(process.env.PWD );
 
-app.use(express.static(path.join(process.cwd(), '/../public'), {maxAge: 86400000}));
+app.use(express.static(path.join(process.cwd(), '../../public'), {maxAge: 86400000}));
 
 app.get('*', function (req, res) {
-    var file = path.join(process.cwd(), 'public/index.html');
-    return res.sendfile(path.resolve(file));
+    var file = path.join(process.cwd(), '../../public/index.html');
+    return res.sendFile(path.resolve(file));
 });
 
 
