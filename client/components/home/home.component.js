df91ad58818a557a0e5eec5a8cf7e1a79e059081
Matthieu Defrenne <matthieu.defrenne@gmail.com>
2017-01-18 20:27:57 +0100

    navbar

--- client/components/home/home.component.js
+++ client/components/home/home.component.js
@@ -15,5 +15,22 @@ angular.module('myApp.homeComponent', ['ngRoute'])
 
 function homeComponent($scope, $location) {
 
+    $(document).ready(function () {
+        if ($(this).scrollTop() > 600) {
+            $('.navbar').fadeIn();
+        } else {
+            $('.navbar').fadeOut();
+        }
+
+        $(function () {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 600) {
+                    $('.navbar').fadeIn();
+                } else {
+                    $('.navbar').fadeOut();
+                }
+            });
+        });
+    });
 
 }
